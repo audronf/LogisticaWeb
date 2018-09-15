b0bb32ed08dc5aee4de7405f74608b96f5b892e8
Federico <33864194+audronf@users.noreply.github.com>
2018-09-14 21:45:59 -0300

    estado del pedido

--- WebContent/consultaPedidoController.js
+++ WebContent/consultaPedidoController.js
@@ -1,6 +1,19 @@
 var app = angular.module('myApp', []).controller('consultaPedidoController',
 		consultaPedidoController);
 
-function consultaPedidoController($scope, $http, pedido) {
+function consultaPedidoController($scope, $http, $window) {
 	
+	$scope.codigoSeguimiento = localStorage['codigoPedido'];
+	
+	$scope.init = function() {
+		$http({
+			
+			method : 'GET',
+			url : 'http://localhost:8081/consultarPedido/'+ $scope.codigoSeguimiento,
+		}).then(function successCallback(response) {
+			$scope.pedido = response.data;
+		}
+		
+	)};
+
 }
--- WebContent/ingresoCodigoController.js
+++ WebContent/ingresoCodigoController.js
@@ -1,21 +1,28 @@
 var app = angular.module('myApp', []).controller('ingresoCodigoController',
-		['$scope', '$http', '$state','ui.router', ingresoCodigoController])
-;
+		['$scope', '$http'/*, '$state'/*,'ui.router'*/, '$window', ingresoCodigoController]);
 
-function ingresoCodigoController($scope, $http, $state) {
+function ingresoCodigoController($scope, $http, $window/*, $state*/) {
 
 	$scope.consultarCodigo = function() {
-		$http(
-				{
-					method : 'GET',
-					url : 'http://localhost:8081/codigo-seguimiento/'
-							+ $scope.codigoSeguimiento,
-				}).then(function successCallback(response) {
-			$scope.pedido = response.data;
-			$state.go("consultaPedido", {
-				pedido : $scope.pedido
-			});
-		});
+//		$http(
+//				{
+//					method : 'GET',
+//					url : 'http://localhost:8081/consultarPedido/'
+//							+ $scope.codigoSeguimiento,
+//				}).then(function successCallback(response) {
+					
+//			localStorage['estado'] = response.data;
+			localStorage['codigoPedido'] = $scope.codigoSeguimiento;
+					
+			$window.location.href = 'http://localhost:8080/Web/consultaPedido.html';
+			
+			
+//			$location.url('http://localhost:8080/Web/consultaPedido.html');
+//			$state.go("/consultaPedido.html", {
+//				pedido : $scope.pedido
+//			});
+
+//		});
 	}
 
 };
