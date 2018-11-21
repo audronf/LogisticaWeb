e6e69cc46d42c16cc60b1d0abbc4605960ae15b6
Francisco Olivé <franolive97@hotmail.com>
2018-11-20 22:02:19 -0300

    Agrego mapa con ubicación del transportista

--- WebContent/consultaPedidoController.js
+++ WebContent/consultaPedidoController.js
@@ -3,6 +3,9 @@ var app = angular.module('myApp', []).controller('consultaPedidoController',
 
 function consultaPedidoController($scope, $http, $window) {
 	
+	$scope.latitud = -34.6170941;
+	$scope.longitud = -58.3820478;
+	
 	$scope.codigoSeguimiento = localStorage['codigoPedido'];
 	
 	$scope.init = function() {
@@ -15,5 +18,22 @@ function consultaPedidoController($scope, $http, $window) {
 		}
 		
 	)};
+	
+    $scope.initialize = function() {
+        $scope.map = new google.maps.Map(document.getElementById('map_div'), {
+           center: {lat: $scope.latitud, lng: $scope.longitud},
+           zoom: 16
+        });
+        
+        var marker = new google.maps.Marker({
+    	    position: {lat: $scope.latitud, lng: $scope.longitud},
+    	    map: $scope.map,
+    	    title: "Ubicación distribuidor"
+    	});
+     }
+     
+     google.maps.event.addDomListener(window, 'load', $scope.initialize);
+     
+    
 
 }
